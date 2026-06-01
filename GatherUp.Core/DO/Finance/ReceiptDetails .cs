6e24652b285240c74364e3f244aca7231835d502
sarabrie <sarabr0278@gmail.com>
2026-06-01 15:02:59 +0300

    add polls files

--- GatherUp.Core/DO/Finance/ReceiptDetails .cs
+++ GatherUp.Core/DO/Finance/ReceiptDetails .cs
@@ -8,5 +8,9 @@ namespace GatherUp.Core.DO.Finance
 {
     internal class ReceiptDetails
     {
+        public string ReceiptNumber { get; set; }
+        public decimal Amount { get; set; } 
+        public bool HasRecipt { get; set; }
+        public List<ReceiptDetails> Receipts { get; set; } = new List<ReceiptDetails>();
     }
 }
